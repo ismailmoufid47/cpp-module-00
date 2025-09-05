292220cf7891b989d8f036c1c22c6981dd04697a
ismailmoufid47 <{"message":"Not Found","documentation_url":"https://docs.github.com/rest/users/emails#list-email-addresses-for-the-authenticated-user","status":"404"}>
2025-09-05 21:54:26 +0100

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	   std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string currentArg = argv[i];
+			int currentArgLength = currentArg.length();
+			for (int i = 0; i < currentArgLength; i++)
+				std::cout << static_cast<char>(toupper(currentArg[i]));
+			std::cout << std::endl;
+		}
+	}
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Contact {
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+    
+}
